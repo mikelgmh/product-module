cd68c290e0d32d48825b6914a2d3486d1392d3e1
Imanol Montoro <imanolma90@gmail.com>
2020-12-09 21:13:08 +0100

    Corregido OrderProduct

--- models/OrderProduct.py
+++ models/OrderProduct.py
@@ -3,8 +3,8 @@ from odoo import models
 class  Order_product (models.Model):
     _name= 'product-module.orderProduct'
     
-    order_id = fields.Many2one('product-module.product', required=True)
-    product_id = fields.Many2one('product-module.order', required=True)
+    order_id = fields.Many2one('product-module.order', required=True)
+    product_id = fields.Many2one('product-module.product', required=True)
     
     total_price= fields.Float(string="Total price of the Order")
     total_quantity=fields.Float(string="Total quantity of products")
