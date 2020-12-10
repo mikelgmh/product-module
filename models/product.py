702add17371ea077ae674ea1746b98f759a4a17c
Aketza19 <aketzae5@gmail.com>
2020-12-10 14:00:00 +0100

    Modelo finalizado

--- models/product.py
+++ models/product.py
@@ -5,8 +5,25 @@
 from odoo import models
 
 class  Product (models.Model):
+    # The name of the module in Odoo.
     __name= 'product-module.product'
-    
-    weight = fields.Float(string="Product weight", required=True)
-    price = fields.Double(string="Price of the product", required=True)
-    name = fields.String (string="Name of the product", required=True)
+    # The weight of the product.
+    weight = fields.Float(
+    string="Product weight")
+    # The price of the product.
+    price = fields.Double(
+    string="Price of the product")
+    # The name of the product.
+    name = fields.String (
+    string="Name of the product", 
+    required=True)
+    # The id of the user that have the product.
+    user_id = fields.Many2One(
+    'res.users', 
+    string="Users", 
+    required=True)
+    # The order that contains this product.
+    order = fields.One2Many(
+    'product-module.order_product',
+    'product_id',
+    string="Order")
