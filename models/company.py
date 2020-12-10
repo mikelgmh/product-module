1b4adcaaa35cd5b2a5e476ef59988ab4425fa7f7
Iker <theofficialdork@hotmail.com>
2020-12-10 14:14:36 +0100

    Update company.py

--- models/company.py
+++ models/company.py
@@ -4,23 +4,21 @@ from odoo import models, fields, api
 
 class Company(models.Model):
     # Reference to the odoo's company model to extends.
+    _name = 'res.company'
     _inherit = 'res.company'
 
+    # The remaining attributes are defined in res.company.
     # The company type. It can be ADMIN, PROVIDER or CLIENT.
     company_type = fields.Selection(
         string='Company type',
         selection=[('1', 'ADMIN'), ('2', 'PROVIDER'), ('3', 'CLIENT')],
         required=True
     )
-    # The amount of users that contain the company.
-    n_users = fields.Integer(
-        string='Amount of users',
-        default=0
-    )
 
-    # The One2Many relation to the project-module.user.
+    # The One2Many relation to the res.users.
     user_id = fields.One2many(
         string='Users',
-        comodel_name='project-module.user',
-        inverse_name='' # TODO: relational column of the opposite model
+        comodel_name='res.users',
+        inverse_name='company_id',
+        ondelete='restrict'
     )
