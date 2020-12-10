a3705e75135b75ea9ec826436b69c9f1158c1dfc
Mikel <mikelgmh@gmail.com>
2020-12-10 13:16:30 +0100

    lastAccess, lastPasswordChange y privilege

--- models/user.py
+++ models/user.py
@@ -1,4 +1,19 @@
-from odoo import models, fields, api
+from odoo import models
 
-class User(models.Model):
-    __name = 'product-module.user'
+class  User(models.Model):
+    _inherit = "res.users";
+    
+
+    lastAccess = fields.Datetime(string="Last access",default=fields.Datetime.now())
+    lastPasswordChange = fields.Datetime(string="Last access",default=null)
+    #Enum Privilege
+    privilege = fields.Selection(
+        selection=[
+        ('1','SUPERUSER'),
+        ('3','WORKER'),
+        ('2','PROVIDER')],
+        string="privilege",
+        required=True)
+
+
+    
