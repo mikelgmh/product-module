baad52d986f4115a716cf3480c13c5d324015f19
Iker <theofficialdork@hotmail.com>
2021-02-01 10:03:15 +0100

    Evaluacion de SGE

--- models/company.py
+++ models/company.py
@@ -2,6 +2,8 @@
 from odoo import models, fields, api, exceptions
 
 # AUTHOR: IKER DE LA CRUZ
+
+
 class Company(models.Model):
     # Reference to the odoo's company model to extends.
     _name = 'res.company'
@@ -15,23 +17,34 @@ class Company(models.Model):
         required=True
     )
     # The amount of bosses in the company.
-    company_bosses = fields.Integer(
-        string='Amount of bosses'
+    number_of_employees = fields.Integer(
+        string='Amount of employees',
+        readonly=True,
+        compute='_number_of_employees'
     )
 
     # The One2Many relation to the res.users.
-    user_id = fields.One2many(
+    employees = fields.One2many(
         string='Users',
         comodel_name='res.users',
         inverse_name='company_id',
         ondelete='restrict'
     )
 
-    # The constrains to the company_bosses It validates if the amount of users
-    # is more than 10.
-    @api.constrains('company_bosses')
+    # The constrains to the number_of_employees It validates if the amount of
+    # users is less than 100.
+    @ api.constrains('number_of_employees')
     def _check_bosses(self):
         for r in self:
-            if r.company_bosses < 10:
+            if r.number_of_employees > 100:
                 raise exceptions.ValidationError(
-                    "Insufficient bosses: %s" % r.company_bosses)
+                    "More employees than expected (<100): %s"
+                    % r.number_of_employees
+                )
+
+    # The multi to the count the number of employees and insert into the
+    # number_of_employees field.
+    @api.multi
+    def _number_of_employees(self):
+        for data in self:
+            data.number_of_employees = len(data.employees)
